91190ce23fd3d9b4527c22c2f58cad46ecebbaa5
Wargreimon501 <imperialdramon_01@hotmail.com>
2019-09-22 22:20:52 -0500

    agrege la tarea

---
+++ libro/problemas_resueltos/capitulo1/ejercisio1_5.py
@@ -0,0 +1,5 @@
+RADIO=float(input("dame el radio del cilindro"))
+ALTU=float(input("dame la altura del cilindro"))
+VOL=3.141592*(RADIO**2)*ALTU
+ARE=2*3.141592*RADIO*ALTU
+print(f"el volumen del cilindo con radio {RADIO} y altura {ALTU} es {VOL} y el area del cilindor con radio {RADIO} y altura {ALTU} es {ARE}")
