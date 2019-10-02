0eed2df451a0880361425dc7b09317f07c0ad298
Wargreimon501 <imperialdramon_01@hotmail.com>
2019-10-01 21:59:45 -0500

    para estudiar

---
+++ libro/problemas_resueltos/capitulo2/ejemplo2_5.py
@@ -0,0 +1,16 @@
+VAL=int(input("dame un numero entero:"))
+A=1
+B=2
+C=3
+if VAL==A:
+    VAL1=A**3
+    print(f"la accion1 es {VAL1} ")
+if VAL==B:
+    VAL2=B**3
+    print(f"la accion2 es {VAL2}")
+if VAL==C:
+    VAL3=C**3
+    print(f"la accion3 es {VAL3}")  
+if VAL==VAL and VAL!=A and VAL!=B and VAL!=C:
+    VALx=VAL**2
+    print(f"la accion x es {VALx}")
