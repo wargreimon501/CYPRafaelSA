265ae6545a638ccc43dd3842d5aad3d5a6095e91
Wargreimon501 <imperiladrmon_01hotmail.com>
2019-10-22 17:41:39 -0500

    problemas

---
+++ listas.py
@@ -0,0 +1,23 @@
+# lista
+lluvias_norte=[80,60,120,100,70,150,100,47,95,70,100,130]
+
+for indice in range(0,12,1):
+    print(f"mes {indice+1} en region norte={lluvias_norte[indice]}")
+print(lluvias_norte[4])
+print(lluvias_norte[:5:])
+
+sueldos = []
+suma=0
+
+for indice in range(7):
+    sueldos.append(int(input("sueldo:")))
+
+print(sueldos)
+for indice in range(7):
+    suma+=sueldos[indice]
+promedio=suma/7
+cont=0
+for indice in range(7):
+    if sueldos[indice]> promedio:
+        cont+=1
+        print(f"arriba:{sueldos[indice]}")
