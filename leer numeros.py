83f941bd092880f098354c376819ebe9213e5931
Wargreimon501 <imperialdramon_01@hotmail.com>
2019-11-14 09:35:29 -0600

    tarea

--- leer numeros.py
+++ leer numeros.py
@@ -29,3 +29,15 @@ archivo=open("numeros.txt","rt")
 numeros2=archivo.readline()
 print(numeros2)
 archivo.close()
+lista_num2=[]
+for linia in numeros2.split('\n'):
+    for numero in linia.split(','):
+        lista_num2.append(int("numero"))
+
+print("-------")
+print(lista_num2)
+print("------------")
+lista_num.sort()
+
+print(f"el mayor es:{lista_num2[-1]}y el menor es:{lista_num2[0]}")
+
---
+++ letras_en_colores.py
@@ -0,0 +1,11 @@
+import text color
+from text color import text_color
+from stringcolor import*
+
+
+print ( default_yellow ( " Universidad Autónoma de México " ))
+print ( default_blue ( " FES Aragón " ))
+print ( default_green ( " Sena Arrieta Rafael " ))
+print ( cs ( " Universidad Autónoma de México " , " oro " ))
+print ( cs ( " FES Aragón " , " azul " ))
+print ( cs ( " Sena Arrieta rafael " , " verde " )
